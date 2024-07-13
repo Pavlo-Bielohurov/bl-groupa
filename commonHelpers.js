1477fb001c95b28eeea928d7971ef02f074610c1
SilviaZhdanska <silviaalbert1992@gmail.com>
2024-07-13 17:33:19 +0000

    Deploying to gh-pages from @ Pavlo-Bielohurov/bl-groupa@ed79b4e46959063aa162f7238d5d68316f809605 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,16 +1,13 @@
-import{a as f}from"./assets/vendor-bdb8a163.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function c(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=c(e);fetch(e.href,t)}})();const a=f.create({baseURL:"https://dummyjson.com"});async function d(){return await a.get("/products")}async function u(o){return await a.get(`/products/${o}`)}function m(o){return o.map(({title:r,images:c,description:n,rating:e,price:t,id:i,brand:s,stock:p})=>`<li data-product="${i}">
-      <img src="${c[0]}" alt="${r}" width="300" />
-      <h2>Brand:${s}</h2>
+import{a as p}from"./assets/vendor-bdb8a163.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&i(c)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const s=p.create({baseURL:"https://dummyjson.com"});async function u(){return await s.get("/products")}async function m(r){return await s.post("/products/add",r)}function f(r){return r.map(({title:o,images:n,description:i,rating:e,price:t,id:c,brand:a,stock:l})=>`<li data-product="${c}">
+      <img src="${n[0]}" alt="${o}" width="300" />
+      <h2>Brand:${a}</h2>
       <p>Price:${t}</p>
       <p>Rating:${e}</p>
-      <p>Quantity:${p}</p>
-      <p>Description:${n}</p>
- </li>`).join("")}function g({title:o,images:r,description:c,rating:n,price:e,id:t,brand:i,stock:s}){return`<div data-product="${t}">
-      <img src="${r[0]}" alt="${o}" width="300" />
-      <h2>Brand:${i}</h2>
-      <p>Price:${e}</p>
-      <p>Rating:${n}</p>
-      <p>Quantity:${s}</p>
-      <p>Description:${c}</p>
- </div>`}d().then(console.log);const h=document.querySelector("#allProducts");d().then(({data:{products:o}})=>{const r=m(o);h.insertAdjacentHTML("afterbegin",r)});const l=document.querySelector("#singleProductForm"),y=document.querySelector("#singleProduct");l.addEventListener("submit",$);function $(o){o.preventDefault();const r=o.target.elements.id.value;u(r).then(console.log),u(r).then(({data:c})=>{const n=g(c);y.innerHTML=n}),l.reset()}
+      <p>Quantity:${l}</p>
+      <p>Description:${i}</p>
+ </li>`).join("")}function y({description:r,price:o,title:n}){return`
+  <h2>Brand:${n}</h2>
+      <p>Price:${o}</p>
+      <p>Description:${r}</p>
+      `}u().then(console.log);document.querySelector("#allProducts");u().then(({data:{products:r}})=>{f(r)});document.querySelector("#singleProductForm");document.querySelector("#singleProduct");const d=document.querySelector("#newProductForm"),g=document.querySelector("#newProductSection");d.addEventListener("submit",P);function P(r){r.preventDefault();const o=r.target.elements.title.value,n=r.target.elements.price.value,i=r.target.elements.description.value,e={title:o,price:n,description:i};console.log(e),m(e).then(({data:t})=>{const c=y(t);g.innerHTML=c,d.reset()})}
 //# sourceMappingURL=commonHelpers.js.map
